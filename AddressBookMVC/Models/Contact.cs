b8c3d749f31ef99aa96768989432459427bb3fa5
tj-longshore <76498230+tj-longshore@users.noreply.github.com>
2022-01-19 13:09:45 -0500

    * Contact.cs: * ContactsController.cs:
    * AddressBookMVC.csproj: Created AddressBookMVC Project

---
+++ AddressBookMVC/Models/Contact.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AddressBook.Models
+{
+    public class Contact
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Address 1")]
+        public string Address1 { get; set; }
+        [Display(Name = "Address 2")]
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        public int Zip { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        public DateTime Created { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Upload Image")]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFile { get; set; }
+        [Display(Name = "Image")]
+        public byte[] ImageData { get; set; }
+        public string ImageType { get; set; }
+
+        public int Id { get; set; }
+
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
